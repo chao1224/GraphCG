cc4420c0df70d10dc15f90867b591aa5e2f6db78
chao1224 <shengchao1224@gmail.com>
2024-07-16 19:08:29 -0400

    Update README and setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='GraphCG',
+      description='',
+      author='Shengchao Liu',
+      author_email='shengchao1224@gmail.com',
+      license='MIT',
+      packages=find_packages())
