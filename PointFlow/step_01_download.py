618954d2c2732bea9d713e68540edf377eee0977
chao1224 <shengchao.hacker@gmail.com>
2024-07-18 14:07:36 -0400

    update address

--- PointFlow/step_01_download.py
+++ PointFlow/step_01_download.py
@@ -2,6 +2,10 @@ import gdown
 
 if __name__ == "__main__":
     # https://github.com/stevenygd/PointFlow/blob/master/README.md?plain=1#L48
-    url = 'https://drive.google.com/uc?id=1sw9gdk_igiyyt7MqALyxZhRrtPvAn0sX'
+    url = "https://drive.google.com/uc?id=1sw9gdk_igiyyt7MqALyxZhRrtPvAn0sX"
     output = "ShapeNetCore.v2.PC15k.zip"
+    gdown.download(url, output)
+
+    url = "https://drive.google.com/uc?id=1CxAaliKyzJSWqgg5Lqw3HXuWHLWQ4EDr"
+    output = "pretrained_models.zip"
     gdown.download(url, output)
